4772baecc54d8ec3806b32d46713bf920dd45006
Cvet Home Desktop <cvet84@gmail.com>
2018-04-03 23:35:17 -0400

    Update test script

--- devlopment/test_script.R
+++ devlopment/test_script.R
@@ -7,3 +7,9 @@ ga_load_settings()
 ga_set_url()
 galog$url
 ga_delete_settings()
+
+ga_initialize(tracking_id="UA-113884719-2",consent=TRUE,hostname="test.test.com")
+user <- ga_set_user_id(user_id="Testy")
+user <-ga_init_user(user_id="Testy")
+ga_collect_pageview(page="/hello", user_id = user)
+ga_collect_event(user=user,event_category = "Ping",event_action = "I touched you")
